abc39784e84b55a8266a376f01ad4055e0cf8adf
64070145Rosjana <63070145@kmitl.ac.th>
2022-09-12 02:49:44 +0700

    new lab52

---
+++ week5part2/src/main/java/com/example/week5part2/Week5part2Application.java
@@ -0,0 +1,13 @@
+package com.example.week5part2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Week5part2Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Week5part2Application.class, args);
+    }
+
+}
