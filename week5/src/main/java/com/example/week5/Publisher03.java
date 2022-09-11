5e20e226a4aae8c9be976f2f6792b7dc17d827bb
64070145Rosjana <63070145@kmitl.ac.th>
2022-09-11 16:44:37 +0700

    new lab5

---
+++ week5/src/main/java/com/example/week5/Publisher03.java
@@ -0,0 +1,21 @@
+package com.example.week5;
+
+import org.springframework.amqp.rabbit.core.RabbitTemplate;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class Publisher03 {
+
+    @Autowired
+    private RabbitTemplate rabbitTemplate;
+    @RequestMapping(value = "/queueUp/{name}", method = RequestMethod.GET)
+    public String queueUp(@PathVariable("name") String name){
+        System.out.println("Publisher In : " + name );
+        rabbitTemplate.convertAndSend("MyFanoutExchange","",name);
+        return "Success";
+    }
+}
