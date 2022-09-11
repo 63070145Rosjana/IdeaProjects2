071e557c642314de66b5c28633f988806ddb10e8
64070145Rosjana <63070145@kmitl.ac.th>
2022-09-12 04:06:14 +0700

    new lab52

--- week5part2/src/main/java/com/example/week5part2/MyView2.java
+++ week5part2/src/main/java/com/example/week5part2/MyView2.java
@@ -3,6 +3,8 @@ package com.example.week5part2;
 import com.vaadin.flow.component.button.Button;
 import com.vaadin.flow.component.combobox.ComboBox;
 import com.vaadin.flow.component.formlayout.FormLayout;
+import com.vaadin.flow.component.orderedlayout.HorizontalLayout;
+import com.vaadin.flow.component.orderedlayout.VerticalLayout;
 import com.vaadin.flow.component.textfield.TextField;
 import com.vaadin.flow.router.Route;
 
@@ -23,7 +25,8 @@ public class MyView2 extends FormLayout {
     private ComboBox<String> labelComboBox2;
 
 
-    public MyView2(){
+    public MyView2() {
+
         btnAddGood = new Button("Add Good Words");
         btnAddBad = new Button("Add Bad Words");
         btnAddSen = new Button("Add Sentence");
@@ -41,5 +44,25 @@ public class MyView2 extends FormLayout {
         tfShowBad.setLabel("Bad Sentences");
         labelComboBox1.setLabel("Good Words");
         labelComboBox2.setLabel("Bad Words");
+
+        tfAddWord.setWidth("500px");
+        tfAddSen.setWidth("500px");
+        tfShowGood.setWidth("500px");
+        tfShowBad.setWidth("500px");
+        labelComboBox1.setWidth("500px");
+        labelComboBox2.setWidth("500px");
+        btnAddGood.setWidth("500px");
+        btnAddBad.setWidth("500px");
+        btnAddSen.setWidth("500px");
+        btnShowSen.setWidth("500px");
+
+        HorizontalLayout hl = new HorizontalLayout();
+        VerticalLayout vl1 = new VerticalLayout();
+        VerticalLayout vl2 = new VerticalLayout();
+        vl1.add(tfAddWord, btnAddGood, btnAddBad, labelComboBox1, labelComboBox2);
+        vl2.add(tfAddSen, btnAddSen, tfShowGood, tfShowBad, btnShowSen);
+        hl.add(vl1, vl2);
+        this.add(hl);
+
     }
 }
