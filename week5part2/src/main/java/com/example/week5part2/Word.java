8e8f5cb98b03a94c6da287dd28670dab3d4f6538
64070145Rosjana <63070145@kmitl.ac.th>
2022-09-13 00:44:29 +0700

    new lab52

--- week5part2/src/main/java/com/example/week5part2/Word.java
+++ week5part2/src/main/java/com/example/week5part2/Word.java
@@ -6,7 +6,7 @@ public class Word {
     public ArrayList<String> badWords = new ArrayList<>();
     public ArrayList<String> goodWords = new ArrayList<>();
     public Word(){
-        badWords.add("fu");
+        badWords.add("fuck");
         badWords.add("olo");
         goodWords.add("happy");
         goodWords.add("enjoy");
