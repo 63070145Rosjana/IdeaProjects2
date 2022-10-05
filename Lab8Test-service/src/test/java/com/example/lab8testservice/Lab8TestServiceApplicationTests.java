d6031e807bc35ed646d986b24fff2786bf87d825
64070145Rosjana <63070145@kmitl.ac.th>
2022-10-05 22:06:26 +0700

    lab8Test

---
+++ Lab8Test-service/src/test/java/com/example/lab8testservice/Lab8TestServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.lab8testservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Lab8TestServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
