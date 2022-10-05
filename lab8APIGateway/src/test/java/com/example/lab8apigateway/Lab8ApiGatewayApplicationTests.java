ec446d1b6d4ff52c916a975de637b61de001334b
64070145Rosjana <63070145@kmitl.ac.th>
2022-10-05 22:07:17 +0700

    lab8API

---
+++ lab8APIGateway/src/test/java/com/example/lab8apigateway/Lab8ApiGatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.lab8apigateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Lab8ApiGatewayApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
